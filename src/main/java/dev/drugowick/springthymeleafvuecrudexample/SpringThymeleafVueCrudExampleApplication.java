0cc874d3c49fef6f3901c29ff52cc92f518d192c
Bruno Drugowick <bruno.drugowick@gmail.com>
2020-02-03 20:18:53 -0300

    Initial project from Spring Initializr (no custom code).

---
+++ src/main/java/dev/drugowick/springthymeleafvuecrudexample/SpringThymeleafVueCrudExampleApplication.java
@@ -0,0 +1,13 @@
+package dev.drugowick.springthymeleafvuecrudexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringThymeleafVueCrudExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringThymeleafVueCrudExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/drugowick/springthymeleafvuecrudexample/SpringThymeleafVueCrudExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.drugowick.springthymeleafvuecrudexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringThymeleafVueCrudExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
