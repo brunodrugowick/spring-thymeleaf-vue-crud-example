38ab65f0c570cb0730a8580242535e64e6ae4f10
Bruno Drugowick <bruno.drugowick@gmail.com>
2020-02-03 23:56:34 -0300

    Adds VueJS POST via Axios for role.
    SURPRISINGLY EASY AGAIN! Added a controller method to handle the POST HTTP request and on the VueJS side of things:
    
    - Added v-on property on the form to indicate the function to call and prevent page refresh.
    - Added v-model to bind input with VueJS date variable.
    - Added related data to VueJS script.
    - Added a POST request via Axios that updates the roles upon completion.

--- src/main/java/dev/drugowick/springthymeleafvuecrudexample/web/api/v1/RolesController.java
+++ src/main/java/dev/drugowick/springthymeleafvuecrudexample/web/api/v1/RolesController.java
@@ -2,9 +2,7 @@ package dev.drugowick.springthymeleafvuecrudexample.web.api.v1;
 
 import dev.drugowick.springthymeleafvuecrudexample.domain.Role;
 import dev.drugowick.springthymeleafvuecrudexample.infrastructure.RoleRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +19,10 @@ public class RolesController {
     @GetMapping("roles")
     public List<Role> list() {
         return roleRepository.findAll();
+    }
+
+    @PostMapping("roles")
+    public Role save(@RequestBody Role role) {
+        return roleRepository.save(role);
     }
 }
